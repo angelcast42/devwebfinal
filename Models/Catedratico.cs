0996671fabf2ff413cae97ff61e45d1fa87d1548
alumno <alumno@LABSERVER>
2021-11-14 18:45:13 -0600

    Agregar archivos de proyecto.

---
+++ Models/Catedratico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devwebfinal.Models
+{
+    public class Catedratico
+    {
+        public int Idcatedratico { get; set; }
+        public string Primer_nombre { get; set; }
+        public string Segundo_nombre { get; set; }
+        public string Primer_apellido { get; set; }
+        public string Segundo_apellido { get; set; }
+        public string Estado { get; set; }
+        public string Aprobado { get; set; }
+    }
+}
